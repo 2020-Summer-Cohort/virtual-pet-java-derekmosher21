2f377570fa69a54712fba9362484c0ec70d991dd
Derek Mosher <derekmosher21@gmail.com>
2020-05-15 16:02:37 -0400

    added system outputs

--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,11 +1,27 @@
 package virtual_pet;
 
+import java.util.Scanner;
+
 public class VirtualPetApplication {
 
     public static void main(String[] args) {
-        //Interact with a VirtualPet object in this method
+        Scanner scanner = new Scanner(System.in);
+        VirtualPet virtualPet = new VirtualPet();
+        boolean gameRun = true;
+        while (gameRun) {
+
+            System.out.println("What do you want to do?");
+            System.out.println("1. Feed Martin");
+            System.out.println("2. Water Martin");
+            System.out.println("3. Play with Martin");
+            System.out.println("4. Put Martin to bed");
+            System.out.println("5. Do Nothing");
+
+            int getAction = input.nextline;
+
+        }
+
         //will have a loop. a while loop
-        while (true) {
         }
 
 
