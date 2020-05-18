429020d21efeaccd3cd700b41a7fb15239e9ca2b
Derek Mosher <derekmosher21@gmail.com>
2020-05-17 20:20:22 -0400

    added switch case statements

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -1,28 +1,29 @@
 package virtual_pet;
 
 public class VirtualPet {
+    private int hunger = 5;
+    private int thirst = 5;
+    private int happiness = 5;
+    private int energy = 5;
+
+    int getHunger() {
+        return hunger;
+    }
+    int getThirst() {
+        return thirst;
+    }
+    int getHappiness() {
+        return happiness;
+    }
+    int getEnergy() {
+        return energy;
+    }
+
+}
+
     void tick(){
         // Will need to replace the voids with something else. They need to return something!
         // Void does not return anything.
     }
 
-    void getHunger(){
-
-    }
-
-    void getThirst(){
-
-    }
-
-    void getWaste(){
-
-    }
-
-    void getBoredom(){
-
-    }
-
-    void getPlay(){
-
-    }
 }
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -3,30 +3,64 @@ package virtual_pet;
 import java.util.Scanner;
 
 public class VirtualPetApplication {
+    static Scanner scanner = new Scanner(System.in);
+    static VirtualPet virtualPet = new VirtualPet();
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        VirtualPet virtualPet = new VirtualPet();
-        boolean gameRun = true;
-        while (gameRun) {
+        gameLoop (scanner);
 
-            System.out.println("What do you want to do?");
-            System.out.println("1. Feed Martin");
-            System.out.println("2. Water Martin");
-            System.out.println("3. Play with Martin");
-            System.out.println("4. Put Martin to bed");
-            System.out.println("5. Do Nothing");
-
-            int getAction = input.nextline;
-
+    }
+    public static void gameLoop(scanner){
+        while(virtualPet.getHunger() > 0 && virtualPet.getThirst() > 0) {
+            giveUserOptions(scanner);
         }
-
-        //will have a loop. a while loop
-        }
-
-
+        gameOver();
 
 
     }
+    public static void gameOver() {
+        int hunger = virtualPet.getHunger();
+        int thirst = virtualPet.getThirst();
+        int happiness = virtualPet.getHappiness();
+
+
+        if(happiness >= 20) {
+            System.out.println("You have made Martin very happy! You win!");
+        } else if (hunger >= 20) {
+            System.out.println("Martin was hungry and ran away. You lose!");
+        } else if (thirst >=20) {
+            System.out.println("Martin was thirsty and ran away. You lose!");
+        }
+    }
+    public static void giveUserOptions (Scanner scanner)  {
+        System.out.println("What do you want to do?");
+        System.out.println("1. Feed Martin");
+        System.out.println("2. Water Martin");
+        System.out.println("3. Play with Martin");
+        System.out.println("4. Put Martin to bed");
+        System.out.println("5. Do Nothing");
+        int userResponse = scanner.nextInt();
+
+        switch (userResponse) {
+            case 1:
+                virtualPet.feedCat();
+                break;
+            case 2:
+                virtualPet.waterCat();
+                break;
+            case 3:
+                virtualPet.playWithCat();
+                break;
+            case 4:
+                virtualPet.putCatToBed();
+                break;
+            default:
+                virtualPet.doNothing();
+        }
+
+    }
+
+
 
 }
+
